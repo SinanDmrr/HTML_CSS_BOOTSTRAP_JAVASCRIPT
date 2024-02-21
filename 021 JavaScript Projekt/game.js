733752767dbeb4c568c356725d70771a42fbdd9e
SinanDmrr <sinan-demir@hotmail.com>
2024-02-21 15:10:33 +0100

    Punktesystem Implementiert: - Problem: Punkte wurden mit jedem klick auf "Neue Runde #reset" mit hoch gezählt. -Lösung das Problem lag daran das mit jedem Reset über MainGame() ein neues EVENT gestartet wurde, wodurch man bei einem Sieg für alle geöffneten EVENTS punkte erhielt -Beispiel: 5 mal auf Neue Rundegeklickt = 5 Events Aktiv 1 Sieg mit 1 Punkt wird dann 5 mal gutgeschrieben
    - Gelöst mit  $(square[element]).off("click");  bevor man $(square[element]).on("click", function(){
                let clickedColor = colors[element];
                pruefeSpiel(clickedColor, this);

--- 021 JavaScript Projekt/game.js
+++ 021 JavaScript Projekt/game.js
@@ -7,6 +7,9 @@ let colors = colorArray(squareNumb);
 // HTML Text RGB ändern in die zu gesuchten RGB Farbe
 let searchingRgbCode = pickColor();
 $('#rgbCode').text(`${searchingRgbCode}`)
+// Erreichte Punkte
+let punkte = 0;
+$("#punkte").text(punkte);
 
 // Um nicht vordefinierte Farben zu nutzen werden mit diese Funktion zufällige Farben generiert
 function randomColors(){
@@ -19,9 +22,9 @@ function randomColors(){
 }
 
 // Erstellt ein Array mit der Anzahl = numSquares
-function colorArray(numSquares){
+function colorArray(squareNumb){
     let colors = [];
-    for(let color = 0; color < numSquares; color++){
+    for(let color = 0; color < squareNumb; color++){
         colors.push(randomColors());
     }
     return colors;
@@ -36,13 +39,19 @@ function pickColor(){
 function pruefeSpiel(clickedColor, element){
     // Überprüfen, ob die ausgewählte Farbe korrekt ist (ersetze dies durch deine Logik)
     if (searchingRgbCode === clickedColor) {
-        alert(`RICHTIG!`);
+        $("#gameheader").css({
+            backgroundColor: searchingRgbCode,
+            fontSize: "30px"
+        })
+        //Chaining / Verkettete Methodenaufrufe
+        $("#rgbCode").fadeOut("slow").fadeIn("fast").fadeOut("slow").fadeIn("fast").html(`RICHTIG ERRATEN!<br>${searchingRgbCode}`);
         // Nicht mehr weiter klickbar machen -> Click Event entfernen
         $(".square").off("click");
-        // Seite neu laden
-        location.reload();
-    } else {
-        alert(`Leider Falsch, versuch es noch einmal!\nGesuchte Zahl: ${searchingRgbCode}\nDeine Zahl: ${clickedColor}`);
+        punkte++;
+        $("#punkte").text(punkte);
+    } 
+    else {
+        //alert(`Leider Falsch, versuch es noch einmal!\nGesuchte Zahl: ${searchingRgbCode}\nDeine Zahl: ${clickedColor}`);
         //$(this).remove(); // Löscht das Square
         // Ändert den Background in die Body Background
         $(element).css({   //This bezieht sich auf das geklickte element
@@ -51,28 +60,39 @@ function pruefeSpiel(clickedColor, element){
     }        
 }
 
-// Durch die Schleife den Squars die Farben geben aus dem Colors-Array
-for (let element = 0; element < square.length; element++){
-    square[element].style.backgroundColor = colors[element];
-    // Squars Klickbar machen
-    $(square[element]).on("click", function(){
-        let clickedColor = colors[element];
-        pruefeSpiel(clickedColor, this);
-        // // Überprüfen, ob die ausgewählte Farbe korrekt ist (ersetze dies durch deine Logik)
-        // if (searchingRgbCode === clickedColor) {
-        //     alert(`RICHTIG!`);
-        //     // Nicht mehr weiter klickbar machen -> Click Event entfernen
-        //     $(".square").off("click");
-        //     // Seite neu laden
-        //     location.reload();
-        // } else {
-        //     alert(`Leider Falsch, versuch es noch einmal!\nGesuchte Zahl: ${searchingRgbCode}\nDeine Zahl: ${clickedColor}`);
-        //     //$(this).remove(); // Löscht das Square
-        //     // Ändert den Background in die Body Background
-        //     $(this).css({   //This bezieht sich auf das geklickte element
-        //         backgroundColor: "#232323"
-        //     })
-        // }        
-    })
+function mainGame(){    
+    for (let element = 0; element < square.length; element++){
+        square[element].style.backgroundColor = colors[element];
+        /*
+         ACHTUNG: Bevor man ein Event mit .on öffnet sollte man mit .off sicher gehen das keine weiteren EVENTS offen sind
+         PROBLEM WAS DARAUS RESULTIERT IST: Das man mehrere Events offen hat mit Punkte++ sodass wenn man dann einmal richtig
+         liegt man mehrere Punkte gut geschrieben bekommt.
+        BEISPIEL: wenn man .off weg lässt und mehr mals das mainGame über den #reset Button startet werden mehrere Events gestartet
+                    und alle warten ob man den Richtigen Wert anklickt denn dann wird über pruefeSpiel() der True zweig abgelaufen
+                    und hier wird ja punkte um 1 erhöht, da man aber jetzt durch mehrmals klicken auf #reset mehrere EVENT-HANDLER
+                    gestaret hat z.B. 5 Stück durch 5 mal klicken erhält man nun für jedes Event 1 Punkt d.h. 5 Punkte was am ende
+                    die Punkte vergabe verfälscht.
+         Aus diesem Grund ist es wichtig den Eventhandler zu schließen bevor man den erneut öffnet
+        */
+        $(square[element]).off("click"); 
+        // Squars Klickbar machen
+        $(square[element]).on("click", function(){
+            let clickedColor = colors[element];
+            pruefeSpiel(clickedColor, this);        
+        })
+    }
 }
 
+mainGame();
+
+
+// NEW GAME Button CLicked
+$("#reset").on("click", function(){
+    colors = colorArray(squareNumb);
+    searchingRgbCode = pickColor();
+    $("#rgbCode").text(searchingRgbCode);
+    $("#gameheader").css({
+        backgroundColor:"#232323"
+    })    
+    mainGame();
+})
